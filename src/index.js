b1fd74b9b4598e2de69f2222c1d19d62bb458077
brandontyruspetty <tyrus1974@gmail.com>
2023-02-16 16:57:18 -0800

    update new index for react 17

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './App.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+ document.getElementById('root')
+);
+
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -20,4 +32,4 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
